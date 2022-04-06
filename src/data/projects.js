288025f792e201c4cf656c8895807db6d36f59bb
jordi <jordi.lanau@yahoo.com>
2022-04-06 15:31:01 +0100

    Added projects data

---
+++ src/data/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    id: 1,
+    github: 'https://github.com/AidKool/tech-blog',
+    live: 'https://a-techiers-blog.herokuapp.com/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254879/tech-blog_fqxtnh.jpg',
+    header: 'Tech Blog',
+    description: 'Full Stack MVC Technology Blog',
+  },
+  {
+    id: 2,
+    github: 'https://github.com/AidKool/jobs-finder/',
+    live: 'https://aidkool.github.io/jobs-finder/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254881/jobs-finder_inx7ui.jpg',
+    header: 'Jobs Finder',
+    description: 'Job search application powered by the Reed API',
+  },
+  {
+    id: 3,
+    github: 'https://github.com/AidKool/weather-dashboard/',
+    live: 'https://aidkool.github.io/weather-dashboard/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254890/weather-dashboard_xl66xo.jpg',
+    header: 'Weather Dashboard',
+    description: 'Weather Dashboard powered by the OpenWeather API',
+  },
+  {
+    id: 4,
+    github: 'https://github.com/AidKool/daily-planner/',
+    live: 'https://aidkool.github.io/daily-planner/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254890/daily-planner_uqrlhh.jpg',
+    header: 'Daily Planner',
+    description: 'Daily Planner application build using Bootstrap and Jquery',
+  },
+  {
+    id: 5,
+    github: 'https://github.com/AidKool/javascript-quiz/',
+    live: 'https://aidkool.github.io/javascript-quiz/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254888/javascript-quiz_wo39kl.jpg',
+    header: 'JavaScript Quiz',
+    description: 'JavaScript quiz built using vanilla JavaScript',
+  },
+  {
+    id: 6,
+    github: 'https://github.com/AidKool/password-generator',
+    live: 'https://aidkool.github.io/password-generator/',
+    image: 'https://res.cloudinary.com/dxe6c1nwr/image/upload/v1649254891/password-generator_hl64to.jpg',
+    header: 'Password Generator',
+    description: 'Secure password generator',
+  },
+];
+
+export default projects;
