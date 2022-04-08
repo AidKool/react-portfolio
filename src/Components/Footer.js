15158dca1e4ccb7aa58d6f7c8ee79677add5beee
jordi <jordi.lanau@yahoo.com>
2022-04-08 18:31:22 +0100

    Style Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+import '../assets/scss/footer.scss';
+
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <div className="container block py-4">
+      <div className="container py-4 is-flex is-flex-direction-column is-align-items-center">
+        <div className="social-media is-flex">
+          <FaGithub className="is-size-3 has-text-black is-clickable social-media-icon" />
+          <FaLinkedin className="is-size-3 has-text-link is-clickable social-media-icon" />
+        </div>
         <p className="has-text-centered is-size-7">{year} &copy; All Rights Reserved</p>
         <a className="is-size-7 has-text-centered is-block" href="https://www.freepik.com/vectors/man">
           Man vector created by studiogstock - www.freepik.com
