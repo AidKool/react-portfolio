217201e023fc3477c9894f9510505d023fcc2e0b
jordi <jordi.lanau@yahoo.com>
2022-04-10 23:22:15 +0100

    Fix spacing in About page component

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,8 +7,8 @@ import '../assets/scss/about.scss';
 function About() {
   const intro = "Hi, I'm Jordi";
   return (
-    <section className="about-section is-flex is-centered">
-      <div className="container is-flex is-flex-direction-column">
+    <section className="about-section px-4">
+      <div className="container about-container is-flex is-flex-direction-column">
         <section className="columns is-centered intro-container">
           <div className="column is-6-tablet is-flex is-flex-direction-column is-justify-content-center">
             <h1 className="intro my-4">{intro}</h1>
