fd7b186bd4faa6666f4ba9197b1eeafbfb016f96
jordi <jordi.lanau@yahoo.com>
2022-05-23 21:15:49 +0100

    Edit About page

--- client/src/Components/Bio/index.js
+++ client/src/Components/Bio/index.js
@@ -11,18 +11,24 @@ function Bio() {
         I hold a degree in Electronic Engineering from the university <a href="https://www.upc.edu/en">UPC</a> and after
         realising it was not what I wanted to do the rest of my life I got a job in an unrelated field. Being a geek
         I&rsquo;ve always loved computers and video-games so after trying to find my way for a while I decided to take
-        the plunge an enroll in a BSc in Computing and Information Systems through the{' '}
-        <a href="University of London">University of London</a>.
+        the plunge and enroll in a BSc in Computing and Information Systems through the{' '}
+        <a href="University of London">University of London</a> which I recently completed in May 2022.
       </p>
       <p className="mt-2 bio-paragraph">
-        I&rsquo;m now working on my final year project which involves creating a secure digital ID easily verifiable
-        using <a href="https://en.wikipedia.org/wiki/Blockchain">Blockchain</a> technology and easily shared using{' '}
-        <a href="https://en.wikipedia.org/wiki/QR_code">QR codes</a>. In addition, I&rsquo;m also enrolled in a Full
-        Stack Development programme through the <a href="https://www.manchester.ac.uk/">University of Manchester</a>.
+        In addition, I have also recently completed a Full Stack Development Certificate through the{' '}
+        <a href="https://www.manchester.ac.uk/">University of Manchester</a> where I had the opportunity to learn from
+        very talented instructors and work along classmates to build group projects using technologies like the{' '}
+        <span className="has-text-weight-bold">MERN stack</span>, <span className="has-text-weight-bold">GraphQL</span>,
+        and <span className="has-text-weight-bold">JWT tokens</span>.
       </p>
       <p className="mt-2 bio-paragraph">
-        I&rsquo;m now looking for an opportunity to be a part of a team and contribute to building modern, scalable and
-        responsive web applications. I&rsquo;m very excited about the future holds.
+        I&rsquo;m currently learning <span className="has-text-weight-bold">TypeScript</span> and developing a full
+        stack job application tracker built with the <span className="has-text-weight-bold">MERN stack</span>,{' '}
+        <span className="has-text-weight-bold">TypeScript</span>,{' '}
+        <span className="has-text-weight-bold">Material UI</span> and{' '}
+        <span className="has-text-weight-bold">GraphQL</span>. I&rsquo;m looking for an opportunity to be a part of a
+        team and contribute to building modern, scalable and responsive web applications. I&rsquo;m very excited about
+        what the future holds.
       </p>
       <p className="mt-2 bio-paragraph">Please don&rsquo;t hesitate to get in touch!</p>
     </article>
--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,33 +1,12 @@
 import React from 'react';
-
 import Bio from '../../Components/Bio';
 
 import './about.scss';
 
 function About() {
-  const intro = "Hi, I'm Jordi";
   return (
     <section className="about-section px-4">
       <div className="container about-container is-flex is-flex-direction-column">
-        <section className="columns is-centered intro-container">
-          <div className="column is-6-tablet is-flex is-flex-direction-column is-justify-content-center">
-            <h1 className="intro my-4">{intro}</h1>
-            <p className="tagline my-0">
-              a <span className="full-stack">Full Stack Developer</span>
-              <br />
-              based in Manchester
-            </p>
-          </div>
-          <div className="column is-6-tablet">
-            <figure className="profile-picture is-flex is-justify-content-center">
-              <img
-                className="profile-picture"
-                src="https://res.cloudinary.com/dxe6c1nwr/image/upload/q_auto:good/v1649518225/me_o04xb9.png"
-                alt="me"
-              />
-            </figure>
-          </div>
-        </section>
         <section className="is-flex is-justify-content-center">
           <Bio />
         </section>
